d78b3036843517adc6ca97db45289160e97656d2
Maher-Bouzid <bouzid.meher@gmail.com>
2020-02-17 21:28:04 +0100

    removed the midelware for testing

--- src/middleware/verifyTokenUpdatePasswors.js
+++ src/middleware/verifyTokenUpdatePasswors.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 export default function verify({ next, store, to }) {
-  axios
-    .post(`https://prodigy-rbk.herokuapp.com/api/user/verifyEmailPassword/${to.params.token}`)
-    .then(res => {
-      return next();
-    })
-    .catch(err => {
-      return next({
-        name: "forbidden"
-      });
-    });
+  // axios
+  //   .post(`https://prodigy-rbk.herokuapp.com/api/user/verifyEmailPassword/${to.params.token}`)
+  //   .then(res => {
+  //     return next();
+  //   })
+  //   .catch(err => {
+  //     return next({
+  //       name: "forbidden"
+  //     });
+  //   });
   return next();
 }
